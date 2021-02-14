bd663bc4694d35d678370ebec652264aaacddefb
Tomas Hilbert <tomashilbert@hotmail.com>
2021-02-14 01:19:49 -0500

    Fin seccion 3 - diseño

---
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+  styleUrls: [
+    './notpagefound.component.css'
+  ]
+})
+export class NotpagefoundComponent implements OnInit {
+
+  year = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
